4cf80ebb84e41cb3f8694960fdd1d9145dfa7c67
James Coles-Nash <james.coles-nash@clio.com>
2016-10-03 13:08:05 -0700

    Adding support for rspec

--- lib/repeat_test.rb
+++ lib/repeat_test.rb
@@ -1,22 +1,34 @@
 class RepeatTest
-  def run(count = 0, command = "Poop")
-    puts "Count: #{count}"
-    puts "Command: #{command}"
-
+  def run(count, command)
     pass_num = 0
     fail_num = 0
+    failure_message = ""
 
-    for i in 1..count.to_i
+    count.times do |i|
+      remaining = count - (i + 1)
+      start_time = Time.now
       result = `#{command}`
+      estimate = (Time.now - start_time).to_i * remaining
 
-      if $?.success?
-        pass_num += 1
+      if (command.include? "rspec")
+        result_line = result[/.*examples.*/]
+        pass_num += result_line[/\d+/].to_i
+
+        failures = result_line[/(\d+)(?!.*\d)/].to_i
+        fail_num += failures
+        failure_message += result if failures != 0
       else
-        fail_num += 1
+        if $?.success?
+          pass_num += 1
+        else
+          fail_num += 1
+          failure_message += $?.to_s
+        end
       end
+      STDERR.print "\r#{pass_num} passed, #{fail_num} failed, #{estimate}s remaining"
     end
 
-    puts "Passes: #{pass_num}"
-    puts "Fails: #{fail_num}"
+    STDERR.print "\n"
+    STDOUT.puts failure_message if failure_message != ""
   end
 end
